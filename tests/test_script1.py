ece401e1435c0706043bbbc36738c496c330849b
Ranjit Prasad <ranjit.reva@gmail.com>
2026-05-07 17:44:40 +0530

    third commit

--- tests/test_script1.py
+++ tests/test_script1.py
@@ -1,3 +1,8 @@
+from selenium.webdriver import Chrome
 
 def test_script1():
-    print('Hi, Good Afternoon, This is Test Script1')
+    print('Hi, Good Afternoon, this is Test Script1 with selenium')
+    driver = Chrome()
+    driver.get("https://www.google.com/")
+    print(driver.title)
+    driver.quit()
